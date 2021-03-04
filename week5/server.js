d5e4b6e36a794b6d2b434d10fea019f5189a92b0
jiaolyulu <54914804+jiaolyulu@users.noreply.github.com>
2021-03-04 02:19:16 -0500

    update

---
+++ week5/server.js
@@ -0,0 +1,62 @@
+// JavaScript source code
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+var Datastore = require('nedb');
+const { timeStamp } = require('console');
+var db = new Datastore({ filename: 'database.json', autoload: true });
+
+
+var urlencodedBodyParser = bodyParser.urlencoded({ extended: true });
+app.use(urlencodedBodyParser);
+
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static('public'));
+
+var dataList = [];
+
+app.get('/', function (req, res) {
+    res.send("helloworld");
+});
+
+
+app.get('/data', function (req, res) {
+    console.log(req.body);
+    
+
+    var dataToSave = {
+        locLat: req.query.locLat,
+        locLng: req.query.locLng,
+        desLat: req.query.desLat,
+        desLng: req.query.desLng,
+        locName: req.query.locName,
+        desName: req.query.desName,
+        message: req.query.message,
+        timeStamp: Date.now()
+    }
+    db.insert(dataToSave, function(err, newDoc){
+        db.find({}).sort({timeStamp:1}).exec(function(err,docs){
+            res.send(docs);
+            
+        })
+        
+    });
+    
+
+    
+    
+});
+app.get('/ticket', function (req, res) {
+    db.find({}).sort({timeStamp:1}).exec(function(err,docs){
+        console.log(docs);
+        var dataWrapper= {data:docs};
+        res.render("ticket.ejs",dataWrapper);
+    })
+    
+});
+app.listen(130, function () {
+    console.log('This website is in Port 130');
+});
+
